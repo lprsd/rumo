196ec240a06c687b19d1b7c53f073758b94b1707
Lakshman Prasad <lakshman@lakshmanprasad.com>
2012-06-13 15:38:27 +0530

    I guess tabs and spaces.

--- crystalball/app/app_delegate.rb
+++ crystalball/app/app_delegate.rb
@@ -1,8 +1,8 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-  	@window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-  	@window.backgroundColor = UIColor.yellowColor
-  	@window.makeKeyAndVisible
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds) 
+    @window.backgroundColor = UIColor.yellowColor
+    @window.makeKeyAndVisible
   	true
   end
 end
